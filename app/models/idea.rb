e8a5ac96a31eef8df9f56393c47f4bbb09ba355f
Karol Błaszczyk <karol.zygmunt.blaszczyk@gmail.com>
2013-11-10 12:26:15 +0100

    change

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -9,14 +9,13 @@ class Idea < ActiveRecord::Base
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  TIMES = [{value: "< 15min", image: "0px 0px"}, {value: "< 30min", image: "0px 60px"}, {value: "> 30min", image: "0px 120px"}]
-  PLACEMENTS = [{value: "DOM", image: "120px 180px"}, {value: "ZEW.", image: ""}, {value: "INNE", image: ""}]
+  TIMES = [{value: "< 15min", image: "0px 0px"}, {value: "< 30min", image: "0px 120px"}, {value: "> 30min", image: "0px 60px"}]
+  PLACEMENTS = [{value: "DOM", image: "120px 180px"}, {value: "ZEW.", image: "120px 120px"}, {value: "INNE", image: "120px 60px"}]
   CASH = [{value: "FREE", image: "180px 180px"}, {value: "< 20zł", image: "180px 120px"}, {value: "> 20zł", image: "180px 60px"}]
   GROUP = [{value: "1 osoba", image: "-60px 180px"}, {value: "2 osoby", image: "-60px 120px"}, {value: "> 2 osoby", image: "-60px 60px"}]
 
-  COLORS = [["TURQUOISE","#1ABC9C"],["EMERALD","#2ECC71"],["NEPHRITIS" , "#27AE60"],["PETER RIVER", "#3498DB"],["BELIZE HOLE" ,"#2980B9"],["AMETHYST", "#9B59B6"],["WISTERIA", "#8E44AD"],
-            ["WET ASPHALT", "#34495E"],["MIDNIGHT BLUE", "#2C3E50"],["SUN FLOWER", "#F1C40F"],["ORANGE","#F39C12"],["CARROT","#E67E22"],
-            ["PUMPKIN","#D35400"],["ALIZARIN","#E74C3C"],["POMEGRANATE","#C0392B"]]
+  COLORS = [["TURQUOISE","#1ABC9C"],["EMERALD","#2ECC71"],["PETER RIVER", "#3498DB"],["AMETHYST", "#9B59B6"],
+            ["SUN FLOWER", "#F1C40F"],["CARROT","#E67E22"]]
 
 
   FRONT_COLORS = [["#1ABC9C","#16A085"],["#F1C40F","#F39C12"],
